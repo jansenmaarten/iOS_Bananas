de71fa3e765fd0fa7280a6d057ed897516dc0bb0
Maarten Jansen <jansen.maarten@me.com>
2012-12-13 14:38:33 +0100

    Added Banana class, Banana factory

---
+++ Bananas/Banana.m
@@ -0,0 +1,13 @@
+//
+//  Banana.m
+//  Bananas
+//
+//  Created by Maarten Jansen on 13/12/12.
+//  Copyright (c) 2012 iCapps. All rights reserved.
+//
+
+#import "Banana.h"
+
+@implementation Banana
+
+@end
